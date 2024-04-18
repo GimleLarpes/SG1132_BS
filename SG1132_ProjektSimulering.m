a32f0135f85246da2c64ce778e56b928bcf3fb6c
Gimle Larpes <gimlelarpes@gmail.com>
2024-04-18 10:19:53 +0200

    first commit

---
+++ SG1132_ProjektSimulering.m
@@ -0,0 +1,163 @@
+%Simulation engine for rockets and stuff
+clear all
+LOG_FREQUENCY = 0.1;
+
+%Simulation parameters
+SIMULATION_TIME = 1;       %Simulation duration (s)
+SIMULATION_RESOLUTION = 100; %Steps per second (s^-1)
+SIMULATION_GRAVITY = 9.80665; %Assume gravity constant (ms^-2)
+
+dTime = 1/SIMULATION_RESOLUTION;
+
+%Environmental parameters
+e_AtmosphericPressure = 101300.0; % 100kPa
+e_AtmosphericTemperature = 20 + 274.15; % (K)
+e_AtmosphericDensity = e_AtmosphericPressure / (8.31446261815324 * e_AtmosphericTemperature); % kgm^-3
+e_WindVelocity = 0;
+e_WindDirection = 0; % Global Wind Direction (RAD)
+
+e_GroundNormal = [0, 0, 1]; % Normal vector of the ground plane
+
+%Train parameters
+t_MassCargo = 1000.0; % (kg)
+t_MassCart = 500; % (kg)
+t_Length = 5.0; % (m)
+t_Width = 2.0; % (m)
+t_Height = 2.0; % (m)
+t_WheelRadius = 0.5; % (m)
+t_CFriction = 0.18;
+
+%Brake parameters
+t_MassBrake = 4*50; % (kg)
+t_BrakeForce = 100.0; % (N)
+b_CFriction = 0.3;
+
+% Aerodynamics
+t_CDrag = 0.31; % Drag coefficient
+t_Area = t_Width * t_Height; % Frontal crossection area
+
+t_Mass = t_MassCart + t_MassBrake + t_MassCargo;
+t_MassCenter = 0.5 * ((t_MassCargo + t_MassCart) * t_Height) / t_Mass; % (m) Center of mass on the z-axis
+
+%Simulation data
+simulation_data_b = []; % Brake data
+simulation_data_k = []; % Kinematic data
+
+environment_data = [dTime, SIMULATION_GRAVITY, e_AtmosphericPressure, e_AtmosphericTemperature, e_AtmosphericTemperature, e_AtmosphericDensity, [0, 0, 0], e_WindVelocity, e_WindDirection, e_GroundNormal];
+train_data = [t_Mass, t_MassCenter, t_BrakeForce, b_CFriction, t_CFriction, t_WheelRadius];
+brake_state = [0, 0, 0, 0, 0, 0, 0, 0]; %Vector of braking forces, temperature - Top left, Top right, Bottom left, Bottom right
+kinematic_state = [0, 0, 0, 0, 0, 0]; % Pos: X, Y, Z  Velocity: X, Y, Z
+for t=0:SIMULATION_TIME*SIMULATION_RESOLUTION
+    % Update environment
+    environment_data = UpdateEnvironment(kinematic_state(3), environment_data);
+
+    %Brake simulations
+    %moment from uneven braking is ignored, add to N? (not needed for straight travel)
+    brake_state(1:2) = BrakeCalc(brake_state, train_data, environment_data, [t_Length/2, -t_Width/2]);%Front left brake
+
+    %Log brake data
+    simulation_data_b = cat(2, simulation_data_b, [t * dTime; brake_state(1); brake_state(2); brake_state(3); brake_state(4); brake_state(5); brake_state(6); brake_state(7); brake_state(8)]);
+
+
+
+    % Kinematic calc
+    position_vector = [kinematic_state(1), kinematic_state(2), kinematic_state(3)];
+    velocity_vector = [kinematic_state(4), kinematic_state(5), kinematic_state(6)];
+    wind_vector = [environment_data(7), environment_data(8), environment_data(9)];
+    air_velocity_vector = velocity_vector + wind_vector;
+
+    %Forces
+    drag_vector = -(0.5 * environment_data(6) * (air_velocity_vector / (norm(air_velocity_vector) + eps)) * norm(air_velocity_vector)^2 * t_CDrag * t_Area); % Drag
+    brake_vector = [0,0,0];%TOTAL BRAKING FORCE, opposite direction to velocity, moment from uneven braking ignored.
+    force_vector = brake_vector + drag_vector + t_Mass * [0, 0, -SIMULATION_GRAVITY];
+    normal_force_vector = -force_vector .* environment_data(12); % Pro tip- don't fall through the map
+    disp(force_vector)
+    disp(normal_force_vector)%wtf
+    force_vector = force_vector + normal_force_vector;
+
+
+    % Apply forces
+    velocity_vector = velocity_vector + dTime * (force_vector / t_Mass);
+    kinematic_state(4) = velocity_vector(1);
+    kinematic_state(5) = velocity_vector(2);
+    kinematic_state(6) = velocity_vector(3);
+
+
+    % Update position
+    position_vector = position_vector + dTime * velocity_vector;
+    kinematic_state(1) = position_vector(1);
+    kinematic_state(2) = position_vector(2);
+    kinematic_state(3) = position_vector(3);
+
+
+
+    %Log data
+    simulation_data_k = cat(2, simulation_data_k, [t * dTime; kinematic_state(1); kinematic_state(2); kinematic_state(3); kinematic_state(4); kinematic_state(5); kinematic_state(6)]);
+
+    % Debug readout
+    if (mod(t, SIMULATION_RESOLUTION * LOG_FREQUENCY) < 1) && true
+        disp("--------------------------------------------")
+        disp("TIME:  "+(t * dTime)+" s")
+        disp("ENVIRONMENT:")
+        disp("Pressure: "+(environment_data(3)/1000))
+        disp("Temperature: "+(environment_data(4)-273))
+        disp("Atm. Density: "+environment_data(6))
+        disp(newline)
+        disp("KINEMATIC:")
+        disp("Speed: "+norm(velocity_vector))
+        disp(sprintf("Velocity: (%d, %d, %d)", velocity_vector)) %#ok<DSPSP>
+        disp(sprintf("Position: (%d, %d, %d)", position_vector)) %#ok<DSPSP>
+        disp("Drag Force: "+norm(drag_vector))
+        disp("Total Force: "+norm(force_vector))
+
+    end
+
+    %Check if stopped
+    if (norm(velocity_vector) == 0)
+        simulation_duration = t / SIMULATION_RESOLUTION;
+        disp("Simulation Time: "+simulation_duration)
+        break;
+    end
+end
+
+%Readout (not working)
+plotselector = 3;
+hold on
+%PLOTS HERE
+
+
+
+%Functions
+function [brake_state] = BrakeCalc(brake_state, train_data, environment_data, centeroffset_xy) %Calculate braking force
+
+    dTime = environment_data(1);
+    atmospheric_pressure = environment_data(3);
+    atmospheric_temperature = environment_data(4);
+    prev_atmospheric_temperature = environment_data(5);
+    
+
+    b_Force = 0;%BRAKING FORCE
+    b_Temp = 0;%BRAKE TEMP(of the internal brake)?
+
+    brake_state = [b_Force, b_Temp];
+end
+function [environment_data] = UpdateEnvironment(altitude, environment_data) % Calculate as functions of altitude
+    % Source: https://www.grc.nasa.gov/WWW/K-12/airplane/atmosmet.html 
+    air_mm = 0.0289647; % Molar mass of air (kg)
+    atmospheric_temperature = 288.19 - 0.00649 * altitude;
+    atmospheric_pressure = 101290 * (atmospheric_temperature / 288.08)^5.256;
+    atmospheric_density = air_mm * atmospheric_pressure / (8.31446261815324 * atmospheric_temperature);
+
+    wind_vector = environment_data(10) * [cos(environment_data(11)), sin(environment_data(11)), 0];
+    
+    % Write values
+    environment_data(3) = atmospheric_pressure; % Pressure
+    environment_data(5) = environment_data(4); % Old Temperature
+    environment_data(4) = atmospheric_temperature; % Temperature
+    environment_data(6) = atmospheric_density; % Density
+    environment_data(7) = wind_vector(1); % Wind x
+    environment_data(8) = wind_vector(2); % Wind y
+    environment_data(9) = wind_vector(3); % Wind z
+end
+
+
